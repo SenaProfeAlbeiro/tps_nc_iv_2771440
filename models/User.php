891699ad2a19e19ab4558b8f5cbe0347e1078add
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-05-09 19:40:45 -0500

    3. Modelo de Negocio (Diagrama Clases)

---
+++ models/User.php
@@ -0,0 +1,14 @@
+<?php
+    class User{
+        // 1ra Parte: Atributos
+        private $rol_code;
+        private $rol_name;
+        private $user_code;
+        private $user_name;
+        private $user_lastname;
+        private $user_id;
+        private $user_email;
+        private $user_pass;
+        private $user_state;
+    }
+?>
