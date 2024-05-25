ae700bbc96f997262984be404de9c72ef01e4585
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-05-24 19:39:11 -0500

    6. Implementación de las vistas: Roles

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    ob_start();
+    require_once "models/DataBase.php";
+    $controller = isset($_REQUEST['c']) ? $_REQUEST['c'] : "Landing";
+    $route_controller = "controllers/" . $controller . ".php";
+    if (file_exists($route_controller)) {
+        $view = $controller;
+        require_once $route_controller;
+        $controller = new $controller;
+        $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
+        if ($view === 'Landing') {
+            require_once "views/company/header.view.php";
+            call_user_func(array($controller, $action));
+            require_once "views/company/footer.view.php";
+        } else {
+            require_once "views/roles/admin/header.view.php";
+            call_user_func(array($controller, $action));
+            require_once "views/roles/admin/footer.view.php";
+        }
+    } else {
+        header("Location:?");
+    }
+    ob_end_flush();
+?>
